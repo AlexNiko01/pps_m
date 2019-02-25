fa45311a0eba92de6add1089a140ea946efddec4
Oleksandra <o.semenchuk@devcaz.com>
2019-02-25 16:27:13 +0200

    f

---
+++ common/components/helpers/Restructuring.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace common\components\helpers;
+
+use Mpdf\Tag\P;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Class Restructuring
+ * @package backend\helpers
+ */
+class Restructuring
+{
+    /**
+     * @param $ps
+     * @return array
+     */
+    public static function firstCategory($ps)
+    {
+        $currencyKey = [];
+        foreach ($ps as $methods) {
+            foreach ($methods as $method) {
+                foreach ($method as $field) {
+                    $currencyKey = ArrayHelper::merge($currencyKey, $field['currencies']);
+                }
+            }
+        }
+        $currencyKeyUnique = array_unique($currencyKey);
+
+        $newPs = [];
+        foreach ($currencyKeyUnique as $currency) {
+            foreach ($ps as $key => $methods) {
+                foreach ($methods as $method) {
+                    foreach ($method as $field) {
+                        if (empty($field)) {
+                            $newPs[$currency][$key] = $methods;
+                        } elseif (in_array($currency, $field['currencies'])) {
+                            $newPs[$currency][$key] = $methods;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $newPs;
+    }
+
+    public static function secondCategory($ps)
+    {
+        $currencyKey = [];
+        $i = 0;
+        foreach ($ps as $key => $val) {
+            if ($i == 0) {
+                foreach ($val as $k => $v) {
+                    $currencyKey[] = $k;
+                }
+            } else {
+                foreach ($val as $method) {
+                    foreach ($method as $field) {
+                        if (isset($field['currencies'])) {
+                            $currencyKey = array_merge($currencyKey, $field['currencies']);
+                        }
+                    }
+                }
+            }
+            $i++;
+        }
+
+        $currencyKey = array_unique($currencyKey);
+
+        $j = 0;
+        $newPs = [];
+        foreach ($currencyKey as $currency) {
+            foreach ($ps as $key => $methods) {
+                if ($j == 0) {
+                    foreach ($methods as $k => $currencyType) {
+                        foreach ($currencyType as $method) {
+                            foreach ($method as $field) {
+
+                                if (empty($field)) {
+                                    $newPs[$currency][$key] = $methods;
+                                } elseif (isset($field['currencies']) && in_array($currency, $field['currencies'])) {
+                                    $newPs[$currency][$key] = $methods;
+                                }
+                            }
+                        }
+                    }
+
+                } else {
+                    foreach ($methods as $method) {
+                        foreach ($method as $field) {
+                            if (empty($field)) {
+                                $newPs[$currency][$key] = $methods;
+                            } elseif (isset($field['currencies']) && in_array($currency, $field['currencies'])) {
+                                $newPs[$currency][$key] = $methods;
+                            }
+                        }
+                    }
+                }
+                $j++;
+            }
+        }
+
+        return $newPs;
+    }
+
+    public static function thirdCategory($ps, $accountMethods)
+    {
+        $currencyKey = [];
+        foreach ($accountMethods as $key => $val) {
+            $currencyKey[] = strtoupper($key);
+        }
+        $currencyKey = array_unique($currencyKey);
+
+        $newPs = [];
+        foreach ($currencyKey as $currency) {
+            foreach ($ps as $key => $methods) {
+                $newPs[$currency][$key] = $methods;
+            }
+        }
+
+        return $newPs;
+    }
+
+    public static function forEmptyPs($accountMethods)
+    {
+
+        $newPs = [];
+        foreach ($accountMethods as $key => $val) {
+            $newPs[strtoupper($key)] = $val;
+        }
+        return $newPs;
+    }
+}
--- libs/lib_autoload.php
+++ libs/lib_autoload.php
@@ -2,23 +2,23 @@
 function autoload($filename)
 {
     $classNameArr = explode('\\', $filename);
-
-
-    $path = __DIR__ . '/' . $classNameArr[0] . '/pps-' . $classNameArr[1] . '/' . $classNameArr[2];
-
     if (in_array('pps', $classNameArr)) {
-        if (!is_dir($path)) {
-            $filename = $path . '.php';
-        } else {
-            $filename = $path . '/' . $classNameArr[3] . '.php';
+        $path = __DIR__ . '/' . $classNameArr[0] . '/pps-' . $classNameArr[1];
+        if (is_dir($path)) {
+            foreach ($classNameArr as $iteration => $dir) {
+                if ($iteration < 2) {
+                    continue;
+                }
+                if ($dir) {
+                    $path .= '/' . $dir;
+                }
+            }
+        }
+        $path = $path . '.php';
+        if (file_exists($path)) {
+            include_once($path);
         }
     }
-
-    if (file_exists($filename)) {
-        include_once($filename);
-    }
-
-
 }
 
 spl_autoload_register('autoload', true);
