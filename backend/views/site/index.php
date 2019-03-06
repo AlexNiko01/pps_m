aa752190c12615b09eaba03ab87e823779b39ce7
Oleksandra <o.semenchuk@devcaz.com>
2019-03-06 16:43:26 +0200

    f

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -79,3 +79,67 @@ $this->title = 'Dashboard';
     'countOfDepositStatuses' => $countOfDepositStatuses,
     'countOfWithdrawStatuses' => $countOfWithdrawStatuses,
 ]); ?>
+<!--<div class="row">-->
+<!--    <div class="col-lg-6 col-xs-12">-->
+<!--        <div class="transactions">-->
+<!--            <h4 class="text-center">Suspended transactions (count: --><?//= count($pendingTransactions) ?><!--)</h4>-->
+<!--            --><?php //if(!empty($pendingTransactions)): ?>
+<!--                <table class="table table-bordered">-->
+<!--                    <thead>-->
+<!--                    <tr>-->
+<!--                        <th>ID</th>-->
+<!--                        <th>Way</th>-->
+<!--                        <th>PS</th>-->
+<!--                        <th>Buyer ID</th>-->
+<!--                        <th>Status</th>-->
+<!--                        <th>Created At</th>-->
+<!--                    </tr>-->
+<!--                    </thead>-->
+<!--                    <tbody>-->
+<!--                    --><?php //foreach ($pendingTransactions as $tx): ?>
+<!--                        <tr>-->
+<!--                            <td>--><?//= Html::a($tx['id'], ['transaction/view', 'id' => $tx['id']], ['class' => 'btn btn-xs btn-link']) ?><!--</td>-->
+<!--                            <td>--><?//= $tx['way'] ?><!--</td>-->
+<!--                            <td>--><?//= $tx['payment_system'] ?><!--</td>-->
+<!--                            <td>--><?//= $tx['buyer_id'] ?><!--</td>-->
+<!--                            <td>--><?//= $statuses[$tx['status']] ?><!--</td>-->
+<!--                            <td>--><?//= date('Y-m-d H:s:i', $tx['created_at']) ?><!--</td>-->
+<!--                        </tr>-->
+<!--                    --><?php //endforeach; ?>
+<!--                    </tbody>-->
+<!--                </table>-->
+<!--            --><?php //else: ?>
+<!--                <p>Suspended transactions not found!</p>-->
+<!--            --><?php //endif;?>
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="col-lg-6 col-xs-12">-->
+<!--        <div class="merchants">-->
+<!--            <h4 class="text-center">Merchants (count: --><?//= count($txsByMerchant) ?><!--)</h4>-->
+<!--            --><?php //if (!empty($txsByMerchant)):?>
+<!--                <table class="table table-bordered">-->
+<!--                    <thead>-->
+<!--                    <tr>-->
+<!--                        <th>ID</th>-->
+<!--                        <th>Name</th>-->
+<!--                        <th>Count Trxs</th>-->
+<!--                        <th>Created At</th>-->
+<!--                    </tr>-->
+<!--                    </thead>-->
+<!--                    <tbody>-->
+<!--                    --><?php //foreach ($txsByMerchant as $merchant): ?>
+<!--                        <tr>-->
+<!--                            <td>--><?//= $merchant['brand_id'] ?><!--</td>-->
+<!--                            <td>--><?//= $merchant['name'] ?><!--</td>-->
+<!--                            <td>--><?//= $merchant['count'] ?><!--</td>-->
+<!--                            <td>--><?//= date('Y-m-d H:s:i', $merchant['created_at']) ?><!--</td>-->
+<!--                        </tr>-->
+<!--                    --><?php //endforeach; ?>
+<!--                    </tbody>-->
+<!--                </table>-->
+<!--            --><?php //else: ?>
+<!--                <p>Merchants not found!</p>-->
+<!--            --><?php //endif;?>
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
