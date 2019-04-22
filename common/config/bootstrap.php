531bd1d7a7e87b689381a222f05f94e286791e6f
Oleksandra <o.semenchuk@devcaz.com>
2019-04-22 17:00:32 +0300

    doc

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,5 +3,5 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-Yii::$classMap['@vendor\webvimark\modules\UserManagement\controllers\AuthController'] = '@backend/controllers/UserManagement/AuthController';
+Yii::$classMap['vendor\webvimark\modules\UserManagement\controllers\AuthController'] = '@backend/controllers/user-management/AuthController.php';
 
